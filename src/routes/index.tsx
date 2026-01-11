d3b1258904d54ed92fbcea831c09b24c2a760976
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2026-01-11 22:15:58 +0000

    add clock

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Clock from "@/components/Clock";
 
-export const Route = createFileRoute("/")({ component: HomePage });
+export const Route = createFileRoute("/")({
+    component: HomePage,
+    loader: async () => ({
+        date: new Date(),
+    }),
+});
 
 function HomePage() {
-    return <></>;
+    const { date } = Route.useLoaderData();
+    return (
+        <div className="container mx-auto my-16">
+            <h1 className="font-bold text-2xl">The current time is</h1>
+            <Clock initialDate={date} />
+        </div>
+    );
 }
